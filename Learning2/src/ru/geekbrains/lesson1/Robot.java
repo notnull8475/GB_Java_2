7d4babca11fca547cccb17611cd4ddbc7e9bcc41
Dinar Gizzatulin <dinar-arsk@yandex.ru>
2022-04-05 20:18:16 +0300

    первый комит, начало возможного ДЗ_1

---
+++ Learning2/src/ru/geekbrains/lesson1/Robot.java
@@ -0,0 +1,11 @@
+package ru.geekbrains.lesson1;
+
+public class Robot {
+    private static final String name = "Робот";
+    public static void run(){
+        System.out.println(name + " Пробежал");
+    }
+    public static void jump(){
+        System.out.println(name + " Прыгнул");
+    }
+}
