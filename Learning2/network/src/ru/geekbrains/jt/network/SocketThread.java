3486ee80da3e920fc470e936de1e3b4a650e1dac
Dinar Gizzatulin <dinar-arsk@yandex.ru>
2022-05-17 07:51:15 +0300

    убрал ошибку при отключении

--- Learning2/network/src/ru/geekbrains/jt/network/SocketThread.java
+++ Learning2/network/src/ru/geekbrains/jt/network/SocketThread.java
@@ -29,7 +29,9 @@ public class SocketThread extends Thread {
                 listener.onReceiveString(this, socket, msg);
             }
         } catch (IOException e) {
-            listener.onSocketException(this, e);
+            if (!isInterrupted()) {
+                listener.onSocketException(this, e);
+            }
         } finally {
             try {
                 socket.close();
