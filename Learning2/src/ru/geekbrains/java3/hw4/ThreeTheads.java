cbed0fe4f4d5c6518b80047666007959b6406b30
Dinar Gizzatulin <dinar-arsk@yandex.ru>
2022-05-25 21:18:37 +0300

    edited

--- Learning2/src/ru/geekbrains/java3/hw4/ThreeTheads.java
+++ Learning2/src/ru/geekbrains/java3/hw4/ThreeTheads.java
@@ -4,40 +4,5 @@ public class ThreeTheads {
 
     public static void main(String[] args) {
 
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.print("A");
-                System.out.print("B");
-                System.out.print("C");
-            }
-        });
-
-        Thread thread1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.print("A");
-                System.out.print("B");
-                System.out.print("C");
-            }
-        });
-
-        Thread thread2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.print("A");
-                System.out.print("B");
-                System.out.print("C");
-            }
-        });
-
-
-        thread.start();
-        thread1.start();
-        thread2.start();
-
     }
-
-
-
 }
