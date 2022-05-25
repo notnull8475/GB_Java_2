d65d0e6695ce1d5e61fc64b211c50edaf85ba935
Dinar Gizzatulin <dinar-arsk@yandex.ru>
2022-05-25 21:26:40 +0300

    задание 1 по ДЗ

--- Learning2/src/ru/geekbrains/java3/hw4/ThreeTheads.java
+++ Learning2/src/ru/geekbrains/java3/hw4/ThreeTheads.java
@@ -1,41 +1,66 @@
 package ru.geekbrains.java3.hw4;
 
 public class ThreeTheads {
+    private final Object mon = new Object();
+    private volatile char currentLetter = 'A';
 
     public static void main(String[] args) {
+        ThreeTheads threeTheads = new ThreeTheads();
 
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.print("A");
-                System.out.print("B");
-                System.out.print("C");
-            }
-        });
-
-        Thread thread1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.print("A");
-                System.out.print("B");
-                System.out.print("C");
-            }
-        });
-
-        Thread thread2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.print("A");
-                System.out.print("B");
-                System.out.print("C");
-            }
-        });
-
-
-        thread.start();
+        Thread thread1 = new Thread(threeTheads::printA);
+        Thread thread2 = new Thread(threeTheads::printB);
+        Thread thread3 = new Thread(threeTheads::printC);
         thread1.start();
         thread2.start();
-
+        thread3.start();
+    }
+    public void printA() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (currentLetter != 'A') {
+                        mon.wait();
+                    }
+                    System.out.print("A");
+                    currentLetter = 'B';
+                    mon.notifyAll();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void printB() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (currentLetter != 'B') {
+                        mon.wait();
+                    }
+                    System.out.print("B");
+                    currentLetter = 'C';
+                    mon.notifyAll();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void printC() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (currentLetter != 'C') {
+                        mon.wait();
+                    }
+                    System.out.print("C");
+                    currentLetter = 'A';
+                    mon.notifyAll();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 
