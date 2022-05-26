6bf63477c32595e58e82275803b942dcbde4a8d7
Dinar Gizzatulin <dinar-arsk@yandex.ru>
2022-05-26 22:16:14 +0300

    все работает. хочу вытащить имя победителя

--- Learning2/src/ru/geekbrains/java3/hm5/Car.java
+++ Learning2/src/ru/geekbrains/java3/hm5/Car.java
@@ -1,11 +1,18 @@
 package ru.geekbrains.java3.hm5;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
     private Race race;
     private int speed;
     private String name;
 
+    private CyclicBarrier cyclicBarrier;
+    public Semaphore semaphore;
+    static boolean WINNER = false;
     public String getName() {
         return name;
     }
@@ -14,11 +21,13 @@ public class Car implements Runnable {
         return speed;
     }
 
-    public Car(Race race, int speed) {
+    public Car(Race race, int speed, CyclicBarrier cyclicBarrier, Semaphore semaphore) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+        this.cyclicBarrier = cyclicBarrier;
+        this.semaphore = semaphore;
     }
 
     @Override
@@ -27,11 +36,22 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int) (Math.random() * 800));
             System.out.println(this.name + " готов");
+            cyclicBarrier.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
+        }
+        if (!WINNER){
+            WINNER=true;
+            System.out.println(name + " WIN");
+
+        }
+        try {
+            cyclicBarrier.await();
+        } catch (InterruptedException | BrokenBarrierException e) {
+            throw new RuntimeException(e);
         }
     }
 }
--- Learning2/src/ru/geekbrains/java3/hm5/MainClass.java
+++ Learning2/src/ru/geekbrains/java3/hm5/MainClass.java
@@ -1,20 +1,41 @@
 package ru.geekbrains.java3.hm5;
 
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.Semaphore;
+
 public class MainClass {
     public static final int CARS_COUNT = 4;
 
+    static boolean nextFinish = false;
+    static String winner;
     public static void main(String[] args) {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+
+
+        CyclicBarrier startStop = new CyclicBarrier(CARS_COUNT, () -> {
+            if (!nextFinish) {
+                System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+                nextFinish = true;
+            } else {
+                System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+                System.out.println("Победитель - " + winner);
+            }
+        });
+        Semaphore tunnel = new Semaphore(CARS_COUNT / 2);
+
+
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
+
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), startStop, tunnel);
         }
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+
     }
 }
 
