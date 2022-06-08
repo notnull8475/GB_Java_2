4bab9fe5384fcb4c90fe84352cc094d25984365d
Dinar Gizzatulin <dinar-arsk@yandex.ru>
2022-06-08 21:04:56 +0300

    дз 7

--- Learning2/src/main/java/ru/geekbrains/java3/hw7/HomeWork7.java
+++ Learning2/src/main/java/ru/geekbrains/java3/hw7/HomeWork7.java
@@ -1,19 +1,57 @@
 package ru.geekbrains.java3.hw7;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class HomeWork7 {
-/* 1. Создать класс, который может выполнять «тесты»,
-* в качестве тестов выступают классы с наборами методов с аннотациями @Test.
-* Для этого у него должен быть статический метод start(),
-* которому в качестве параметра передается или объект типа Class, или имя класса.
-* Из «класса-теста» вначале должен быть запущен метод с аннотацией @BeforeSuite,
-* если такой имеется, далее запущены методы с аннотациями @Test,
-* а по завершению всех тестов – метод с аннотацией @AfterSuite.
-* К каждому тесту необходимо также добавить приоритеты (int числа от 1 до 10),
-* в соответствии с которыми будет выбираться порядок их выполнения,
-* если приоритет одинаковый, то порядок не имеет значения.
-* Методы с аннотациями @BeforeSuite и @AfterSuite должны присутствовать
-* в единственном экземпляре, иначе необходимо бросить RuntimeException при запуске «тестирования».
-**/
+    @BeforeSuite
+    public void startBeforeSuite() {
+        System.out.println("BeforeSuite");
+    }
+
+    @AfterSuite
+    public void afterSuite() {
+        System.out.println("AfterSuite");
+    }
+
+    @Test(priority = 1)
+    public void forTest1() {
+        System.out.println("Test - 1");
+    }
+
+    @Test(priority = 10)
+    public void forTest2() {
+        System.out.println("Test - 2");
+    }
+
+    @Test
+    public void forTest3() {
+        System.out.println("Test - 3");
+    }
+
+    @Test
+    public void forTest4() {
+        System.out.println("Test - 4");
+    }
+
+    @Test(priority = 3)
+    public void forTest5() {
+        System.out.println("Test - 5");
+    }
 
 
+    public static void main(String[] args) {
+        Tester tester = new Tester();
+        HomeWork7 hm7 = new HomeWork7();
+        try {
+            tester.start(hm7.getClass());
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
